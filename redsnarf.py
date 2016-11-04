0a6aea2c93b276e677f2073f0b0cd20b69f94364
llandeilocymro <edward.d.williams@gmail.com>
2016-11-04 12:44:40 +0000

    Update redsnarf.py

--- redsnarf.py
+++ redsnarf.py
@@ -1,4 +1,7 @@
 #! /usr/bin/python
+# Released as open source by NCC Group Plc - https://www.nccgroup.trust/uk/
+# https://github.com/nccgroup/redsnarf
+# Released under Apache V2 see LICENCE for more information
 
 import os, argparse, signal, sys, re, binascii, subprocess
 
